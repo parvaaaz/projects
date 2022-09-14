3a9ff3a4d9d6c9752264237b50c6bd6ad91b9a21
parvaaaz <nimanazary110@gmail.com>
2022-09-14 06:05:13 +0430

    time added

--- Game-2.py
+++ Game-2.py
@@ -1,12 +1,20 @@
+import time
 print ("\n \n Hello :) \n Let\'s play hoop!!!!! \n You Have 5 seconds for each turn! - Type * instead of hoop! \n " )
 a = input ( " Please insert hoop cycle length : " ) 
 n = 1
+def Timers () :
+        c = 6 
+        while ( c != 0 ) :
+            print ( c )
+            time.sleep(1)
+            c -= 1               
 while (True) :
+    Timers ()
+    b = input ()    
     if ( (n % int(a)) == 0 ) :
         print ( "hoop" ) 
     else :
-        print (n)
-    b = input ()
+        print (n)  
     try : 
         if ( int (b) != (n+1)) : 
             break
